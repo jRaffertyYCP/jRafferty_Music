80c58c547593c608ca3d9c352b2115135d344d7b
Tim McKenna - YCP <47125817+YCPTimMcKenna@users.noreply.github.com>
2020-11-30 06:38:57 -0800

    TIM EDITS - Mobile Styling
    What I did:
    - Setup mobile styles with what you had already (Try these out in DevTools)
    
    What you'll do next:
    - Shop photos
    - Photo Overlay for shop
    - Add to cart function

--- js/main.js
+++ js/main.js
@@ -13,6 +13,16 @@ function myFunction() {
   }
 }
 
-// Timed Content on a page //
+// Off Canvas Navigation //
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
 
 
